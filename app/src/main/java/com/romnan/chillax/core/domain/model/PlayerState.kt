2a09e7b4b371c2348d46ac08cf652c8fb90d8665
Deddy Romnan <romnandeddy@gmail.com>
2022-05-17 11:47:49 +0700

    add todos

--- app/src/main/java/com/romnan/chillax/core/domain/model/PlayerState.kt
+++ app/src/main/java/com/romnan/chillax/core/domain/model/PlayerState.kt
@@ -1,10 +1,9 @@
 package com.romnan.chillax.core.domain.model
 
-import androidx.annotation.StringRes
-import com.romnan.chillax.R
 import kotlinx.collections.immutable.PersistentList
 
 data class PlayerState(
     val phase: PlayerPhase,
     val soundsList: PersistentList<Sound>,
+    // TODO: add moodsList
 )
