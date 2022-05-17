f4ccc11c989e112a4c7a9dba981d93d78135b78e
Deddy Romnan <romnandeddy@gmail.com>
2022-05-17 22:14:47 +0700

    feat: add bottom sheet to control player state

--- app/src/main/java/com/romnan/chillax/core/presentation/MainActivity.kt
+++ app/src/main/java/com/romnan/chillax/core/presentation/MainActivity.kt
@@ -5,19 +5,12 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
-import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.*
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.PauseCircle
-import androidx.compose.material.icons.filled.PlayCircle
-import androidx.compose.material.icons.filled.StopCircle
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.core.content.ContextCompat
 import androidx.lifecycle.lifecycleScope
@@ -25,9 +18,9 @@ import com.ramcosta.composedestinations.DestinationsNavHost
 import com.ramcosta.composedestinations.manualcomposablecalls.composable
 import com.ramcosta.composedestinations.rememberNavHostEngine
 import com.romnan.chillax.NavGraphs
-import com.romnan.chillax.R
 import com.romnan.chillax.core.domain.model.PlayerPhase
 import com.romnan.chillax.core.presentation.component.BottomBar
+import com.romnan.chillax.core.presentation.component.PlayerBottomSheet
 import com.romnan.chillax.core.presentation.service.PlayerService
 import com.romnan.chillax.core.presentation.theme.ChillaxTheme
 import com.romnan.chillax.destinations.MoodsScreenDestination
@@ -69,58 +62,51 @@ class MainActivity : ComponentActivity() {
             ChillaxTheme {
                 val engine = rememberNavHostEngine()
                 val navController = engine.rememberNavController()
+
                 val scaffoldState = rememberScaffoldState()
-                val bottomSheetScaffoldState = rememberBottomSheetScaffoldState(
-                    bottomSheetState = BottomSheetState(BottomSheetValue.Expanded)
-                )
+                val bsState = rememberBottomSheetState(initialValue = BottomSheetValue.Collapsed)
+                val bsScaffoldState = rememberBottomSheetScaffoldState(bottomSheetState = bsState)
+
+                LaunchedEffect(key1 = 1) {
+                    viewModel.playerPhase.collectLatest {
+                        if (it == PlayerPhase.Stopped) {
+                            bsScaffoldState.bottomSheetState.collapse()
+                        }
+                    }
+                }
 
                 Scaffold(
+                    modifier = Modifier.fillMaxSize(),
                     scaffoldState = scaffoldState,
                     bottomBar = { BottomBar(navController) }
                 ) { scaffoldPadding ->
+
+                    val peekHeight =
+                        if (viewModel.playerPhase.collectAsState().value != PlayerPhase.Stopped)
+                            70.dp else 0.dp
+
                     BottomSheetScaffold(
-                        scaffoldState = bottomSheetScaffoldState,
-                        sheetPeekHeight = 64.dp,
-                        modifier = Modifier.padding(scaffoldPadding),
+                        scaffoldState = bsScaffoldState,
+                        sheetPeekHeight = peekHeight,
+                        modifier = Modifier
+                            .padding(scaffoldPadding)
+                            .fillMaxSize(),
                         sheetContent = {
+                            PlayerBottomSheet(
+                                playerPhase = viewModel.playerPhase.collectAsState().value,
+                                bottomSheetState = bsScaffoldState.bottomSheetState,
+                                peekHeight = peekHeight,
+                                onPlayPauseClicked = viewModel::onPlayPauseClicked,
+                                onStopClicked = viewModel::onStopClicked
+                            )
+                        }) { bsScaffoldPadding ->
 
-                            // TODO: separate sheet content into its own composable
-
-                            // TODO: hide bottom sheet when phase == Stopped
-
-                            val playerPhase = viewModel.playerPhase.collectAsState().value
-                            Row(verticalAlignment = Alignment.CenterVertically) {
-                                if (playerPhase != PlayerPhase.Stopped) {
-                                    IconButton(onClick = { viewModel.onPlayPauseClicked() }) {
-                                        Icon(
-                                            imageVector = if (playerPhase == PlayerPhase.Playing)
-                                                Icons.Default.PauseCircle else Icons.Default.PlayCircle,
-                                            contentDescription = stringResource(
-                                                if (playerPhase == PlayerPhase.Playing)
-                                                    R.string.pause else R.string.play
-                                            )
-                                        )
-                                    }
-
-                                    IconButton(onClick = { viewModel.onStopClicked() }) {
-                                        Icon(
-                                            imageVector = Icons.Default.StopCircle,
-                                            contentDescription = stringResource(R.string.stop)
-                                        )
-                                    }
-                                }
-                            }
-                        }) { bottomSheetScaffoldPadding ->
-
-                        // TODO: fix scrolling behavior. Problem: unlimited scrolling
-                        val scrollState = rememberScrollState()
+                        // TODO: fix scrolling behavior.
                         DestinationsNavHost(
                             engine = engine,
                             navController = navController,
                             navGraph = NavGraphs.root,
-                            modifier = Modifier
-                                .padding(bottomSheetScaffoldPadding)
-                                .verticalScroll(scrollState)
+                            modifier = Modifier.padding(bsScaffoldPadding)
                         ) {
                             composable(MoodsScreenDestination) {
                                 MoodsScreen(viewModel = viewModel)
--- app/src/main/java/com/romnan/chillax/core/presentation/theme/Theme.kt
+++ app/src/main/java/com/romnan/chillax/core/presentation/theme/Theme.kt
@@ -5,6 +5,7 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
 
 private val DarkColorPalette = darkColors(
     primary = Purple200,
@@ -35,10 +36,12 @@ fun ChillaxTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composabl
         LightColorPalette
     }
 
-    MaterialTheme(
-        colors = colors,
-        typography = Typography,
-        shapes = Shapes,
-        content = content
-    )
+    CompositionLocalProvider(LocalSpacing provides Spacing()) {
+        MaterialTheme(
+            colors = colors,
+            typography = Typography,
+            shapes = Shapes,
+            content = content
+        )
+    }
 }
