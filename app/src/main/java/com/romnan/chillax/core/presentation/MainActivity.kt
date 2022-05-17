ddae415dbe55d414b7ead0190515ac36143a8381
Deddy Romnan <romnandeddy@gmail.com>
2022-05-17 11:44:05 +0700

    fix: feat add player phase

--- app/src/main/java/com/romnan/chillax/core/presentation/MainActivity.kt
+++ app/src/main/java/com/romnan/chillax/core/presentation/MainActivity.kt
@@ -6,17 +6,27 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
 import androidx.compose.foundation.layout.Row
-import androidx.compose.material.Button
-import androidx.compose.material.Scaffold
-import androidx.compose.material.Text
-import androidx.compose.material.rememberScaffoldState
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.PauseCircle
+import androidx.compose.material.icons.filled.PlayCircle
+import androidx.compose.material.icons.filled.StopCircle
 import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import androidx.core.content.ContextCompat
 import androidx.lifecycle.lifecycleScope
 import com.ramcosta.composedestinations.DestinationsNavHost
 import com.ramcosta.composedestinations.manualcomposablecalls.composable
 import com.ramcosta.composedestinations.rememberNavHostEngine
 import com.romnan.chillax.NavGraphs
+import com.romnan.chillax.R
+import com.romnan.chillax.core.domain.model.PlayerPhase
 import com.romnan.chillax.core.presentation.component.BottomBar
 import com.romnan.chillax.core.presentation.service.PlayerService
 import com.romnan.chillax.core.presentation.theme.ChillaxTheme
@@ -33,15 +43,23 @@ import kotlinx.coroutines.flow.collectLatest
 class MainActivity : ComponentActivity() {
     private val viewModel: MainViewModel by viewModels()
 
+    @OptIn(ExperimentalMaterialApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         lifecycleScope.launchWhenCreated {
-            viewModel.isPlaying.collectLatest { isPlaying ->
+            viewModel.playerPhase.collectLatest { phase ->
                 Intent(this@MainActivity, PlayerService::class.java).also { intent ->
-                    if (isPlaying) ContextCompat
-                        .startForegroundService(this@MainActivity, intent)
-                    else stopService(intent)
+
+                    when (phase) {
+                        PlayerPhase.Playing -> ContextCompat
+                            .startForegroundService(this@MainActivity, intent)
+
+                        PlayerPhase.Paused -> ContextCompat
+                            .startForegroundService(this@MainActivity, intent)
+
+                        PlayerPhase.Stopped -> stopService(intent)
+                    }
                 }
             }
         }
@@ -51,37 +69,63 @@ class MainActivity : ComponentActivity() {
                 val engine = rememberNavHostEngine()
                 val navController = engine.rememberNavController()
                 val scaffoldState = rememberScaffoldState()
+                val bottomSheetScaffoldState = rememberBottomSheetScaffoldState(
+                    bottomSheetState = BottomSheetState(BottomSheetValue.Expanded)
+                )
 
                 Scaffold(
                     scaffoldState = scaffoldState,
-                    topBar = {
-                        Row {
-                            Button(onClick = { viewModel.onPlayPauseClicked() }) {
-                                Text(text = if (viewModel.isPlaying.collectAsState().value) "Pause" else "Play")
-                            }
-
-                            Button(onClick = { viewModel.onStopClicked() }) {
-                                Text(text = "Stop")
-                            }
-                        }
-                    },
                     bottomBar = { BottomBar(navController) }
-                ) {
-                    DestinationsNavHost(
-                        engine = engine,
-                        navController = navController,
-                        navGraph = NavGraphs.root
-                    ) {
-                        composable(MoodsScreenDestination) {
-                            MoodsScreen(viewModel = viewModel)
-                        }
+                ) { scaffoldPadding ->
+                    BottomSheetScaffold(
+                        scaffoldState = bottomSheetScaffoldState,
+                        sheetPeekHeight = 64.dp,
+                        modifier = Modifier.padding(scaffoldPadding),
+                        sheetContent = {
+                            val playerPhase = viewModel.playerPhase.collectAsState().value
+                            Row(verticalAlignment = Alignment.CenterVertically) {
+                                if (playerPhase != PlayerPhase.Stopped) {
+                                    IconButton(onClick = { viewModel.onPlayPauseClicked() }) {
+                                        Icon(
+                                            imageVector = if (playerPhase == PlayerPhase.Playing)
+                                                Icons.Default.PauseCircle else Icons.Default.PlayCircle,
+                                            contentDescription = stringResource(
+                                                if (playerPhase == PlayerPhase.Playing)
+                                                    R.string.pause else R.string.play
+                                            )
+                                        )
+                                    }
 
-                        composable(SoundsScreenDestination) {
-                            SoundsScreen(viewModel = viewModel)
-                        }
+                                    IconButton(onClick = { viewModel.onStopClicked() }) {
+                                        Icon(
+                                            imageVector = Icons.Default.StopCircle,
+                                            contentDescription = stringResource(R.string.stop)
+                                        )
+                                    }
+                                }
+                            }
+                        }) { bottomSheetScaffoldPadding ->
 
-                        composable(SettingsScreenDestination) {
-                            SettingsScreen()
+                        val scrollState = rememberScrollState()
+                        DestinationsNavHost(
+                            engine = engine,
+                            navController = navController,
+                            navGraph = NavGraphs.root,
+                            modifier = Modifier
+                                .padding(bottomSheetScaffoldPadding)
+                                .verticalScroll(scrollState)
+                        ) {
+                            composable(MoodsScreenDestination) {
+                                MoodsScreen(viewModel = viewModel)
+                            }
+
+                            composable(SoundsScreenDestination) {
+                                SoundsScreen(viewModel = viewModel)
+                            }
+
+                            composable(SettingsScreenDestination) {
+                                SettingsScreen()
+                            }
                         }
                     }
                 }
