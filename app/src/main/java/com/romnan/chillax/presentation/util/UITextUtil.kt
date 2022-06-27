245f796986bda1b9fae6789e97cf1e62f68fb5b2
Deddy Romnan <romnandeddy@gmail.com>
2022-06-27 17:06:21 +0700

    refactor: reorganize project

---
+++ app/src/main/java/com/romnan/chillax/presentation/util/UITextUtil.kt
@@ -0,0 +1,21 @@
+package com.romnan.chillax.presentation.util
+
+import android.content.Context
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.stringResource
+import com.romnan.chillax.domain.model.UIText
+
+@Composable
+fun UIText.asString(): String {
+    return when (this) {
+        is UIText.DynamicString -> this.value
+        is UIText.StringResource -> stringResource(id = this.id)
+    }
+}
+
+fun UIText.asString(context: Context): String {
+    return when (this) {
+        is UIText.DynamicString -> this.value
+        is UIText.StringResource -> context.getString(this.id)
+    }
+}
