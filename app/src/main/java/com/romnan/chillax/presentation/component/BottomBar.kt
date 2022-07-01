144e8527a75a97dc7d0c1543c886051e3508d585
Deddy Romnan <romnandeddy@gmail.com>
2022-07-01 22:58:20 +0700

    ui: change background color

--- app/src/main/java/com/romnan/chillax/presentation/component/BottomBar.kt
+++ app/src/main/java/com/romnan/chillax/presentation/component/BottomBar.kt
@@ -1,8 +1,19 @@
 package com.romnan.chillax.presentation.component
 
-import androidx.compose.material.*
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.selection.selectableGroup
+import androidx.compose.material.BottomNavigationItem
+import androidx.compose.material.Icon
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.ramcosta.composedestinations.navigation.navigateTo
 import com.romnan.chillax.presentation.NavGraphs
@@ -18,22 +29,37 @@ fun BottomBar(
     val currentDestination: Destination = navController.appCurrentDestinationAsState().value
         ?: NavGraphs.root.startAppDestination
 
-    BottomNavigation(
-        backgroundColor = MaterialTheme.colors.surface,
+    Row(
+        Modifier
+            .fillMaxWidth()
+            .background(MaterialTheme.colors.surface)
+            .height(56.dp)
+            .selectableGroup(),
+        horizontalArrangement = Arrangement.SpaceBetween,
     ) {
         BottomBarDestination.values().forEach { destination ->
+            val isSelected = currentDestination == destination.direction
+
             BottomNavigationItem(
-                selected = currentDestination == destination.direction,
+                selected = isSelected,
                 onClick = {
                     navController.navigateTo(destination.direction) { launchSingleTop = true }
                 },
                 icon = {
                     Icon(
                         imageVector = destination.icon,
-                        contentDescription = stringResource(destination.label)
+                        contentDescription = stringResource(destination.label),
+                        tint = if (isSelected) MaterialTheme.colors.primary
+                        else MaterialTheme.colors.onSurface.copy(alpha = 0.5f),
                     )
                 },
-                label = { Text(stringResource(destination.label)) },
+                label = {
+                    Text(
+                        text = stringResource(destination.label),
+                        color = if (isSelected) MaterialTheme.colors.primary
+                        else MaterialTheme.colors.onSurface.copy(alpha = 0.5f),
+                    )
+                },
             )
         }
     }
