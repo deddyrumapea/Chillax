7d81ec87b4c3363974123326045daf4c7d26cef7
Deddy Romnan <romnandeddy@gmail.com>
2022-08-29 20:50:13 +0700

    fix: beginning volume level bug

--- app/src/main/java/com/romnan/chillax/data/service/PlayerService.kt
+++ app/src/main/java/com/romnan/chillax/data/service/PlayerService.kt
@@ -3,6 +3,7 @@ package com.romnan.chillax.data.service
 import android.app.Service
 import android.content.Intent
 import android.os.IBinder
+import androidx.annotation.FloatRange
 import androidx.annotation.RawRes
 import androidx.media3.common.MediaItem
 import androidx.media3.datasource.RawResourceDataSource
@@ -52,13 +53,10 @@ class PlayerService : Service() {
 
                 // Add a player for each playing sound
                 player.sounds.forEach { sound ->
-                    when {
-                        !resPlayers.contains(sound.audioResId) ->
-                            addResPlayer(resId = sound.audioResId)
-
-                        resPlayers[sound.audioResId]?.volume != sound.volume ->
-                            changeVolume(resId = sound.audioResId, volume = sound.volume)
-                    }
+                    addResPlayer(
+                        resId = sound.audioResId,
+                        volume = sound.volume,
+                    )
                 }
 
                 when (player.phase) {
@@ -103,18 +101,31 @@ class PlayerService : Service() {
         logcat { "playing all players" }
     }
 
-    private fun addResPlayer(@RawRes resId: Int) {
-        val player = ExoPlayer.Builder(this)
-            .apply { setHandleAudioBecomingNoisy(true) }
-            .build()
-            .apply {
+    private fun addResPlayer(
+        @RawRes resId: Int,
+        @FloatRange(from = 0.0, to = 1.0) volume: Float,
+    ) {
+        when {
+            !resPlayers.contains(resId) -> {
                 val uri = RawResourceDataSource.buildRawResourceUri(resId)
-                setMediaItem(MediaItem.fromUri(uri))
-                repeatMode = ExoPlayer.REPEAT_MODE_ONE
-                prepare()
+                val player = ExoPlayer.Builder(this)
+                    .apply { setHandleAudioBecomingNoisy(true) }
+                    .build()
+                    .apply {
+                        setMediaItem(MediaItem.fromUri(uri))
+                        setVolume(volume)
+                        repeatMode = ExoPlayer.REPEAT_MODE_ONE
+                        prepare()
+                    }
+                resPlayers[resId] = player
+                logcat { "added $resId player, volume: ${player.volume}" }
             }
-        resPlayers[resId] = player
-        logcat { "added $resId player" }
+
+            resPlayers[resId]?.volume != volume -> {
+                resPlayers[resId]?.volume = volume
+                logcat { "changed $resId volume to $volume" }
+            }
+        }
     }
 
     private fun removeResPlayer(@RawRes resId: Int) {
@@ -125,11 +136,6 @@ class PlayerService : Service() {
             resPlayers.remove(resId)
             logcat { "removed $resId player" }
         }
-    }
-
-    private fun changeVolume(resId: Int, volume: Float) {
-        resPlayers[resId]?.volume = volume
-        logcat { "changed $resId volume to $volume" }
     }
 
     override fun onBind(p0: Intent?): IBinder? = null
