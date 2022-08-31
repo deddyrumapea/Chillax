5c7c6e01549c060b3129fb18d8ba40729dc06698
Deddy Romnan <romnandeddy@gmail.com>
2022-08-31 15:49:55 +0700

    fix: assymetrical padding of MoodsScreen title

--- app/src/main/java/com/romnan/chillax/presentation/composable/component/ScreenTitle.kt
+++ app/src/main/java/com/romnan/chillax/presentation/composable/component/ScreenTitle.kt
@@ -1,5 +1,6 @@
 package com.romnan.chillax.presentation.composable.component
 
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
@@ -12,15 +13,16 @@ import com.romnan.chillax.presentation.composable.theme.spacing
 fun ScreenTitle(
     text: String,
     modifier: Modifier = Modifier,
-) = Text(
-    text = text,
-    style = MaterialTheme.typography.h4,
-    fontWeight = FontWeight.Bold,
-    color = MaterialTheme.colors.onBackground,
-    modifier = modifier.padding(
+    paddingValues: PaddingValues = PaddingValues(
         start = MaterialTheme.spacing.medium,
         top = MaterialTheme.spacing.large,
         end = MaterialTheme.spacing.medium,
         bottom = MaterialTheme.spacing.medium,
     ),
+) = Text(
+    text = text,
+    style = MaterialTheme.typography.h4,
+    fontWeight = FontWeight.Bold,
+    color = MaterialTheme.colors.onBackground,
+    modifier = modifier.padding(paddingValues),
 )
