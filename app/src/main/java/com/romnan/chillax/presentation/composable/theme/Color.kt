991038362e90993af5ba4a662c8ed1a163b5e5f6
Deddy Romnan <romnandeddy@gmail.com>
2022-09-05 23:03:48 +0700

    change color scheme

--- app/src/main/java/com/romnan/chillax/presentation/composable/theme/Color.kt
+++ app/src/main/java/com/romnan/chillax/presentation/composable/theme/Color.kt
@@ -2,41 +2,21 @@ package com.romnan.chillax.presentation.composable.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Indigo50 = Color(0xfff3f8ff)
-val Indigo100 = Color(0xffecf1ff)
-val Indigo200 = Color(0xffe2e7fb)
-val Indigo300 = Color(0xffd2d7eb)
-val Indigo400 = Color(0xffaeb3c6)
-val Indigo500 = Color(0xff8e8e93) // Dark Theme Secondary Text
-val Indigo600 = Color(0xff666b7c)
-val Indigo700 = Color(0xff2d344b)
-val Indigo800 = Color(0xff21283f) // Dark Theme Surface
-val Indigo900 = Color(0xff141927) // Dark Theme Background
+val BlueGray50 = Color(0xfff3f8ff) // Light Theme Background
+val BlueGray800 = Color(0xff1e2732) // Dark Theme Surface
+val BlueGray900 = Color(0xff15202b) // Dark Theme Background
 
-val Blue50 = Color(0xffe9ebff)
-val Blue100 = Color(0xffc7cdff)
-val Blue200 = Color(0xff9fadff)
-val Blue300 = Color(0xff718cff)
-val Blue400 = Color(0xff4870ff) // Primary
-val Blue500 = Color(0xff0054fc)
-val Blue600 = Color(0xff004bf0)
-val Blue700 = Color(0xff003fe3)
-val Blue800 = Color(0xff0033d9)
-val Blue900 = Color(0xff001cc0)
+val Blue400 = Color(0xff1d9bf0) // Primary
+val Blue700 = Color(0xff097cd0) // Primary Variant
 
-val Green50 = Color(0xffe5faeb)
-val Green100 = Color(0xffc0f2ce)
-val Green200 = Color(0xff93eaae)
-val Green300 = Color(0xff5be18c)
-val Green400 = Color(0xff00d971) // Secondary
-val Green500 = Color(0xff00d054)
-val Green600 = Color(0xff00c049)
-val Green700 = Color(0xff00ad3c)
-val Green800 = Color(0xff009b2f)
-val Green900 = Color(0xff007a19)
+val Gold700 = Color(0xfffebc00)
 
-val Orange800 = Color(0xffff9c41)
+val Pink400 = Color(0xfff91880)
 
-val Red400 = Color(0xffff2d55)
+val LightBlue400 = Color(0xff7856ff)
 
-val catBgColors = listOf(Blue400, Green400, Orange800, Red400)
+val Orange900 = Color(0xffff7a00)
+
+val DarkGreen400 = Color(0xff00ba7c)
+
+val catBgColors = listOf(Blue400, DarkGreen400, Orange900, LightBlue400, Pink400, Gold700)
