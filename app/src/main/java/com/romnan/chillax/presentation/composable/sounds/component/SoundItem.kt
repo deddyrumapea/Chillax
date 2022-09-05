9a49eca726e4cac6ef3164e17482a91782450b58
Deddy Romnan <romnandeddy@gmail.com>
2022-09-05 21:36:14 +0700

    improve composable performance

--- app/src/main/java/com/romnan/chillax/presentation/composable/sounds/component/SoundItem.kt
+++ app/src/main/java/com/romnan/chillax/presentation/composable/sounds/component/SoundItem.kt
@@ -5,16 +5,20 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.*
+import androidx.compose.material.Divider
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.material.contentColorFor
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.drawBehind
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
 import com.romnan.chillax.presentation.composable.theme.spacing
 import com.romnan.chillax.presentation.model.SoundPresentation
 import com.romnan.chillax.presentation.util.asString
@@ -50,10 +54,15 @@ fun SoundItem(
                 .aspectRatio(1f)
                 .fillMaxWidth()
         ) {
-            Icon(
-                painter = painterResource(id = sound().iconResId),
-                tint = contentColorFor(backgroundColor = boxColor.value),
+            val iconColor = animateColorAsState(
+                if (sound().isSelected) contentColorFor(backgroundColor = selectedColor())
+                else MaterialTheme.colors.onSurface
+            )
+
+            AsyncImage(
+                model = sound().iconResId,
                 contentDescription = null,
+                colorFilter = ColorFilter.tint(color = iconColor.value),
                 modifier = Modifier.size(24.dp),
             )
         }
