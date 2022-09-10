301b1642d3a4bd9ddcef148386cf766b1da9939b
Deddy Romnan <romnandeddy@gmail.com>
2022-09-10 17:16:22 +0700

    improve ui design
    - change attribution text
    - change instruction text
    - add close button to settings dialog

--- app/src/main/java/com/romnan/chillax/presentation/composable/settings/component/SettingsDialog.kt
+++ app/src/main/java/com/romnan/chillax/presentation/composable/settings/component/SettingsDialog.kt
@@ -1,18 +1,26 @@
 package com.romnan.chillax.presentation.composable.settings.component
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Divider
+import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Dialog
 import androidx.compose.ui.window.DialogProperties
+import com.romnan.chillax.R
 import com.romnan.chillax.presentation.composable.theme.spacing
 
 @Composable
@@ -33,13 +41,24 @@ fun SettingsDialog(
                     shape = RoundedCornerShape(8.dp),
                 )
         ) {
-            Text(
-                text = title(),
-                style = MaterialTheme.typography.h6,
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
                 modifier = Modifier
                     .fillMaxWidth()
                     .padding(MaterialTheme.spacing.medium),
-            )
+            ) {
+                Text(
+                    text = title(),
+                    style = MaterialTheme.typography.h6,
+                    modifier = Modifier.weight(1f),
+                )
+
+                Icon(
+                    imageVector = Icons.Default.Close,
+                    contentDescription = stringResource(id = R.string.cd_close),
+                    modifier = Modifier.clickable { onDismissRequest() },
+                )
+            }
 
             Divider()
 
