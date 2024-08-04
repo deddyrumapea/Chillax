67b4ff890b0f756813aae795b121d67aded06b86
Deddy Romnan <romnandeddy@gmail.com>
2024-08-04 18:18:35 +0700

    fix: revamp mood item appearance

--- app/src/main/java/com/romnan/chillax/presentation/composable/moods/MoodsState.kt
+++ app/src/main/java/com/romnan/chillax/presentation/composable/moods/MoodsState.kt
@@ -6,6 +6,5 @@ import com.romnan.chillax.domain.model.Player
 data class MoodsState(
     val player: Player? = null,
     val moods: List<Mood> = emptyList(),
-    val customMoodIdToShowDeleteButton: String? = null,
     val customMoodToDelete: Mood? = null,
 )
