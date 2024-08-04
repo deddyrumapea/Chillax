c26f5d3d557bacaddc0b3a99019d65b15d90d2aa
Deddy Romnan <romnandeddy@gmail.com>
2024-08-04 11:59:41 +0700

    feat: add playing mood indicator

--- app/src/main/java/com/romnan/chillax/presentation/composable/moods/MoodsState.kt
+++ app/src/main/java/com/romnan/chillax/presentation/composable/moods/MoodsState.kt
@@ -1,8 +1,10 @@
 package com.romnan.chillax.presentation.composable.moods
 
 import com.romnan.chillax.domain.model.Mood
+import com.romnan.chillax.domain.model.Player
 
 data class MoodsState(
+    val player: Player? = null,
     val moods: List<Mood> = emptyList(),
     val customMoodIdToShowDeleteButton: String? = null,
     val customMoodToDelete: Mood? = null,
