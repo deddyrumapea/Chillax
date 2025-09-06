4623fb5146ef88fe18626ea183bee05d796994c9
Deddy Rumapea <romnandeddy@gmail.com>
2025-09-06 15:44:38 +0700

    Refactor: Update train and plane sounds and increase default volume
    This commit introduces the following changes:
    - Renamed "Jet Plane" sound to "Plane Cabin" and "Train" sound to "Train Cabin".
    - Updated associated sound files, icons, and string resources.
    - Removed "City" and "Other" sound categories.
    - Moved "Train Cabin" and "Plane Cabin" sounds to the "Cosy" category.
    - Increased `DEFAULT_SOUND_VOLUME` from 0.7f to 0.8f.

--- app/src/main/java/com/romnan/chillax/domain/constant/PlayerConstants.kt
+++ app/src/main/java/com/romnan/chillax/domain/constant/PlayerConstants.kt
@@ -2,5 +2,5 @@ package com.romnan.chillax.domain.constant
 
 object PlayerConstants {
     const val MAX_SOUNDS = 8
-    const val DEFAULT_SOUND_VOLUME = 0.7f
+    const val DEFAULT_SOUND_VOLUME = 0.8f
 }
